dfacf14123e385924d972a3a3e57cd58fa18aff2
emonsimply <emonsimply@gmail.com>
2025-11-11 11:01:22 +0600

    Added /my-foods endpoint to fetch foods by donor email

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const express = require('express')
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
-const app = express()
-const port = process.env.PORT || 3000
-
-
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.td2q5ge.mongodb.net/?appName=Cluster0`;
 
@@ -21,12 +17,11 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    
     await client.connect();
 
     const db = client.db("food-db");
@@ -61,6 +56,11 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/my-foods", async (req, res) => {
+      const email = req.query.email;
+      const result = await foodCollection.find({ donator_email: email }).toArray()
+      res.send(result);
+    });
 
     app.post("/foods", async (req, res) => {
       const data = req.body;
@@ -71,11 +71,10 @@ async function run() {
       });
     });
 
-
-
-    
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -83,20 +82,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-
-
-
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
